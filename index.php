65e7bf6f026a6d82f19b5746459f22d7e0bb1800
Carlos Campderrós <carlos@campderros.com>
2012-09-04 22:05:04 +0200

    visually more attractive (not much though)

--- index.php
+++ index.php
@@ -28,15 +28,30 @@ if (isset($_GET['expr'])) {
 }
 
 ?>
-<form method="get" action="">
-	<label>Regex to test: <input type="text" name="expr" value="<?php if (isset($expr)) echo htmlspecialchars($expr, ENT_QUOTES, 'utf-8');?>"/></label>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title>Testing regular expressions</title>
+		<link rel="stylesheet" href="styles.css"/>
+	</head>
+	<body>
+		<h1>Test your regular expression</h1>
+		<h2>Sample input</h2>
+		<pre><?php echo htmlspecialchars(file_get_contents($file), ENT_QUOTES, 'utf-8');?></pre>
+		<form method="get" action="">
+			<label>Regex to test: <input type="text" name="expr" value="<?php if (isset($expr)) echo htmlspecialchars($expr, ENT_QUOTES, 'utf-8');?>"/></label>
 <?php if (isset($error)) echo "<em class='error'>$error</em>\n";?>
-	<input type="submit" value="Test Regex" />
-</form>
-<pre><?php echo htmlspecialchars(file_get_contents($file), ENT_QUOTES, 'utf-8');?></pre>
+			<input type="submit" value="Test Regex" />
+		</form>
 <?php
 if (!empty($output)) {
 	echo "<h2>Matching lines</h2>\n";
 
 	echo "<pre>", show_output($output), "</pre>";
+} else if (!empty($expr)) {
+	echo "<p>No matches...</p>\n";
 }
+?>
+	</body>
+</html>
